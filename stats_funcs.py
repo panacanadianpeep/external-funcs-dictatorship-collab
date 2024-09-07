7951fc1ad7c512a276a7535a073b6f45b7c97948
DESKTOP-B55FE6C\Seb Admin <sebastian.williamson.vega@gmail.com>
2024-09-07 17:08:52 +0200

    New functions of histograms part 1

--- stats_funcs.py
+++ stats_funcs.py
@@ -1,6 +1,15 @@
 import numpy as np
 import pandas as pd
+import matplotlib as plt
 
 def ReadCSVfile_edgecaseenter(file_path):
     df = pd.read_csv(file_path, lineterminator='\n', low_memory=False)
     return df
+
+def Convert_Hist(df):
+    df.Hist(column='column_name')
+
+def Draw_Hist(title, xlabel, ylabel):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
