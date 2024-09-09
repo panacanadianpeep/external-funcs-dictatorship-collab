b48d5059523eb22efc1a4c37be35a7edbfcbd7ba
DESKTOP-B55FE6C\Seb Admin <sebastian.williamson.vega@gmail.com>
2024-09-09 12:58:07 +0200

    _DictWraper for pandas dataframe

--- stats_funcs.py
+++ stats_funcs.py
@@ -6,6 +6,10 @@ def ReadCSVfile_edgecaseenter(file_path):
     df = pd.read_csv(file_path, lineterminator='\n', low_memory=False)
     return df
 
+# When we plot Hist, Pmf and Cdf objects, they don't appear in
+# the legend unless we override the default label.
+DEFAULT_LABEL = "_nolegend_"
+
 class _DictWrapper(object):
     def __init__(self, obj=None, label=None):
         """Initializes the distribution.
@@ -13,8 +17,14 @@ class _DictWrapper(object):
         obj: Hist, Pmf, Cdf, Pdf, dict, pandas Series, list of pairs
         label: string label"""
 
-        if isinstance(obj, pd.DataFrame):
-            print(obj, "pandas")
+        self.label = label if label else DEFAULT_LABEL
+        self.d = {}
+
+        if obj == None:
+            return
+
+        if isinstance(obj, pd.Series):
+            self.d.update(obj.value_counts().items())
         elif isinstance(obj, dict):
             print(obj, "dict")
         else:
